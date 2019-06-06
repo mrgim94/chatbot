45eb964814778d2fb3894e4455724709acce2df8
cys779988 <48742393+cys779988@users.noreply.github.com>
2019-06-06 16:03:45 +0900

    modify

--- chatbot.py
+++ chatbot.py
@@ -169,32 +169,20 @@ def get_answer(text, user_key):
 #----------------------------------------------------
 # 피자 정보 처리
 #----------------------------------------------------
-def process_pizza_info(pizza_name):
+def process_menu_info(menu_name):
 
-    if pizza_name == u'불고기피자':
-        answer = '<Photo>http://www.pizzamaru.co.kr/UpFile/Menu/cla8_F_01.jpg</Photo>'
-        answer += '한국의 맛 불고기를 부드러운 치즈와 함께!'
-    elif pizza_name == u'페퍼로니피자':
-        answer = '<Photo>http://www.pizzamaru.co.kr/UpFile/Menu/cla6_F_01.jpg</Photo>'
-        answer += '고소한 페파로니햄이 쫀득한 치즈위로 뜸뿍!'
-    elif pizza_name == u'포테이토피자':
-        answer = '<Photo>http://www.pizzamaru.co.kr/UpFile/Menu/cla7_F_01.jpg</Photo>'
-        answer += '저칼로리 감자의 담백한 맛!'
-
-    return answer
-
-
-
-#----------------------------------------------------
-# 피자 주문 처리
-#----------------------------------------------------
-def process_pizza_order(pizza_name, address):
-
-    answer = pizza_name.encode('UTF-8') + '를 주문하셨습니다.'
-    answer += " '" + address.encode('UTF-8') + "'의 주소로 지금 배달하도록 하겠습니다."
-    answer += ' 이용해주셔서 감사합니다.' 
+    if menu_name == u'라면':
+        answer = '<Photo>https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwKmEtdnNXQ7JLQDD-sjbezog3s6L7ESyaoQs9UnXnLNL6_SFuQ</Photo>'
+        answer += '라면'
+    elif menu_name == u'비빔밥':
+        answer = '<Photo>https://i.ytimg.com/vi/XyPEZpEo4qc/maxresdefault.jpg</Photo>'
+        answer += '비빔밥'
+    elif menu_name == u'돈까스':
+        answer = '<Photo>https://img.siksinhot.com/place/1351044846469397.jpg</Photo>'
+        answer += '돈까스'
 
     return answer
+
 
 
 
@@ -213,13 +201,9 @@ def webhook():
     #--------------------------------
     # 액션 처리
     #--------------------------------
-    if action == 'pizza_info':
-        pizza_name = req['result']['parameters']['pizza_type']
-        answer = process_pizza_info(pizza_name)
-    elif action == 'pizza_order':
-        pizza_name = req['result']['parameters']['pizza_type']
-        address = req['result']['parameters']['address']
-        answer = process_pizza_order(pizza_name, address)
+    if action == 'menu_info':
+        menu_name = req['result']['parameters']['menu_type']
+        answer = process_menu_info(menu_name)
     else:
         answer = 'error'
 
@@ -264,7 +248,7 @@ def message():
     # 답변 구함
     #--------------------------------
     if content == u'대화하기':
-        answer = '안녕하세요! 전 피자 주문을 받는 챗봇입니다~'
+        answer = '안녕하세요! 저는 메뉴알려주는 챗봇입니다'
     else:
         answer = get_answer(content, user_key)
 
